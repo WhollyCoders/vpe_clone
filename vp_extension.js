59d2017cd4c4cb87f7b0621917baff20dd4aed58
Michael L Parks <github@wmi-inc.com>
2018-06-04 14:01:12 -0400

    Re-Factor Code and ADD Comments - Project Complete

--- vp_extension.js
+++ vp_extension.js
@@ -1,5 +1,5 @@
 // Select Output Element
-    defaultOutputText       = "Welcome WhollyCoder..."
+    defaultOutputText       = "Welcome WhollyCoder...";
     outputElement           = document.getElementById("output");
     outputElement.innerHTML = defaultOutputText;
 
@@ -7,13 +7,14 @@
     function hideOutputElement(){
         outputElement.style.visibility = "hidden";
     }
-// Create Function to Display ViewPort Dimensions (displayVPDim)
 
+// Create Function to Display ViewPort Dimensions (displayVPDim)
     function displayVPDim(){
       // Display Output Element
       outputElement.style.visibility = "visible";
+
       // Initialize Variables
-      // innerWidth  = iw // innerHeight = ih // outerWidth  = ow // outerHeight = oh
+      // innerWidth(iw), innerHeight(ih), outerWidth(ow), outerHeight(oh)
       iw = window.innerWidth;
       ih = window.innerHeight;
       ow = window.outerWidth;
@@ -29,3 +30,6 @@
       // Set Timeout to Hide Output Element
       setTimeout(hideOutputElement, 10000);
     }
+
+// Event Listener for Window Resize
+window.addEventListener("resize", displayVPDim);
